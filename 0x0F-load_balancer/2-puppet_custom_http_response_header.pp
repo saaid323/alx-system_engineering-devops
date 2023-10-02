48f050e8d77dec15f855631bea61dd119c051397
saaid323 <saaid2538@gmail.com>
2023-10-02 11:10:29 -0700

    added 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,35 +1,24 @@
 #  automate the task of creating a custom HTTP header response, but with Puppet.
-exec { 'update':
-  command     => 'apt-get -y update',
-  path        => '/usr/bin:/bin',
-  refreshonly => true,
-}
-exec { 'apt-get':
-  command => 'apt-get -y install nginx',
-  path    => '/usr/bin:/bin',
-  require => Exec['update'],
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install'],
 }
 
--> file { '/var/www/html/index.nginx-debian.html':
-  content => 'Hello World!',
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['header'],
 }
 
-file_line { 'redirect':
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'server_name _;',
-  line   => "\tlocation /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}",
-  notify => Exec['restart'],
-}
-
-file_line { 'add_header':
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => "^\tlocation / {",
-  line   => "\t\tadd_header X-Served-By \"${::hostname}\";",
-  notify => Exec['restart'],
+exec { 'header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart'],
 }
 
 exec { 'restart':
-  command     => '/usr/sbin/service nginx restart',
-  refreshonly => true,
-  subscribe   => File_line['redirect'],
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
