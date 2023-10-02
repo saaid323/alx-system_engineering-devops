dab917d621e5f3362a46f1f34af0939a6f878265
saaid323 <saaid2538@gmail.com>
2023-10-02 10:06:35 -0700

    added 2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,37 @@
+#  automate the task of creating a custom HTTP header response, but with Puppet.
+
+{ 'update':
+  command  => 'sudo apt-get -y update',
+  path     => '/usr/bin',
+  logoutput => true,
+  require  => Package['nginx'],
+  before   => Exec['install_nginx'],
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+}
+
+exec { 'install_nginx':
+  command  => 'sudo apt-get -y install nginx',
+  path     => '/usr/bin',
+  logoutput => true,
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  path        => '/usr/bin',
+  logoutput   => true,
+  notify      => Exec['restart_nginx'],
+  require     => Package['nginx'],
+}
+
+exec { 'restart_nginx':
+  command  => 'sudo service nginx restart',
+  path     => '/usr/bin',
+  logoutput => true,
+  refreshonly => true,
+}
+
