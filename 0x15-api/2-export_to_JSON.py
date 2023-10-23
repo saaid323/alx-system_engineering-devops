15aef28d61eaeaac69aed3e3de6cf37dd7defdcb
saaid323 <saaid2538@gmail.com>
2023-10-23 10:26:56 -0700

    added 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,11 +7,12 @@ if __name__ == "__main__":
     num = sys.argv[1]
     users = requests.get(f'https://jsonplaceholder.typicode.com/users/{num}')
     url = f'https://jsonplaceholder.typicode.com/users/{num}/todos'
-    task = requests.get(url) 
+    task = requests.get(url)
     username = users.json()['username']
     dic = {num: []}
     for i in task.json():
-        tasks = {"task": i["title"], "completed": i["completed"], "username": username}
+        tasks = {"task": i["title"], "completed": i["completed"],
+                 "username": username}
         dic[num].append(tasks)
     with open(f"{num}.json", "w") as file:
         json.dump(dic, file)
